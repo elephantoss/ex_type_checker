7a9dee4affc66be00172e1e8ed3a1a16ad1ba6a4
Elephant OSS <spirit@elephantoss.io>
2022-10-18 14:55:36 -0700

    feat: add which?/1 function
    which?/1 returns the type of the variable given to it as argument.

--- lib/type_checker.ex
+++ lib/type_checker.ex
@@ -1,3 +1,68 @@
 defmodule TypeChecker do
-  @moduledoc false
+  @moduledoc """
+  TypeChecker provides one function to check the type of variables.
+
+  It is inspired by stackoverflow.com/questions/28377135/check-typeof-variable-in-elixir thread.
+  """
+
+  @doc """
+  `which?/1` returns the type of a vairable.
+
+  ## Examples
+      iex> TypeChecker.which?(:atom)
+      "atom"
+      iex> bin = "hello"
+      iex> TypeChecker.which?(bin)
+      "binary"
+      iex> bitstr = <<1::3>>
+      iex> TypeChecker.which?(bitstr)
+      "bitstring"
+      iex> TypeChecker.which?(:true)
+      "boolean"
+      iex> pi = 3.14159
+      iex> TypeChecker.which?(pi)
+      "float"
+      iex> fun = fn (a, b) -> a + b end
+      iex> TypeChecker.which?(fun)
+      "function"
+      iex> TypeChecker.which?(&TypeChecker.which?/1)
+      "function"
+      iex> int = 42
+      iex> TypeChecker.which?(int)
+      "integer"
+      iex> list = [1,2,3,4]
+      iex> TypeChecker.which?(list)
+      "list"
+      iex> map = %{:foo => "bar", "hello" => :world}
+      iex> TypeChecker.which?(map)
+      "map"
+      iex> TypeChecker.which?(nil)
+      "nil"
+      iex> pid = spawn(fn -> 1 + 2 end)
+      iex> TypeChecker.which?(pid)
+      "pid"
+      iex> port = Port.open({:spawn, "cat"}, [:binary])
+      iex> TypeChecker.which?(port)
+      "port"
+      iex> ref = :erlang.make_ref
+      iex> TypeChecker.which?(ref)
+      "reference"
+      iex> tuple = {:name, "alex"}
+      iex> TypeChecker.which?(tuple)
+      "tuple"
+  """
+  types =
+    ~w[boolean binary bitstring float function integer list struct map nil pid port reference tuple atom]
+
+  for type <- types do
+    def which?(x) when unquote(:"is_#{type}")(x), do: unquote(type)
+  end
+
+  # No idea how to test this. Do you? ¯\_(ツ)_/¯
+  # coveralls-ignore-start
+  def which?(_) do
+    "unknown"
+  end
+
+  # coveralls-ignore-stop
 end
--- test/type_checker_test.exs
+++ test/type_checker_test.exs
@@ -1,4 +1,90 @@
 defmodule TypeCheckerTest do
+  @moduledoc false
   use ExUnit.Case
   doctest TypeChecker
+
+  defstruct [:foo, :bar]
+
+  describe "which?/1" do
+    test "returns \"atom\" for an :atom" do
+      assert TypeChecker.which?(:atom) == "atom"
+    end
+
+    #  recap: https://elixir-lang.org/getting-started/binaries-strings-and-char-lists.html
+    test "returns \"binary\" when variable is a binary" do
+      string = "hello"
+      assert TypeChecker.which?(string) == "binary"
+    end
+
+    # recap: https://elixir-lang.readthedocs.io/en/latest/intro/6.html#binaries-and-bitstrings
+    test "returns \"bitstring\" when variable is a bitstring" do
+      bitstr = <<1::size(1)>>
+      assert TypeChecker.which?(bitstr) == "bitstring"
+    end
+
+    # "Every binary is a bitstring but every bitstring need not be a binary" ...
+
+    test "returns \"float\" if the value is float" do
+      golden = 1.618
+      assert TypeChecker.which?(golden) == "float"
+    end
+
+    test "returns \"function\" when the variable is a function" do
+      sum = fn a, b -> a + b end
+      assert sum.(2, 3) == 5
+      assert TypeChecker.which?(sum) == "function"
+
+      assert TypeChecker.which?(&TypeChecker.which?/1) == "function"
+    end
+
+    test "list" do
+      list = [1, 2, 3, 4]
+      assert TypeChecker.which?(list) == "list"
+    end
+
+    test "map" do
+      map = %{:foo => "bar", "hello" => :world}
+      assert TypeChecker.which?(map) == "map"
+    end
+
+    test "nil" do
+      assert TypeChecker.which?(nil) == "nil"
+    end
+
+    test "pid" do
+      pid = spawn(fn -> 1 + 2 end)
+      assert TypeChecker.which?(pid) == "pid"
+    end
+
+    # https://hexdocs.pm/elixir/1.12/Port.html
+    test "port" do
+      port = Port.open({:spawn, "cat"}, [:binary])
+      assert TypeChecker.which?(port) == "port"
+    end
+
+    test "reference" do
+      ref = :erlang.make_ref()
+      assert TypeChecker.which?(ref) == "reference"
+    end
+
+    test "tuple" do
+      tuple = {:name, "alex"}
+      assert TypeChecker.which?(tuple) == "tuple"
+    end
+
+    test "integer" do
+      int = 10
+      assert TypeChecker.which?(int) == "integer"
+    end
+
+    test "struct" do
+      struct = %TypeCheckerTest{}
+      assert TypeChecker.which?(struct) == "struct"
+    end
+
+    test "boolean" do
+      bool = true
+      assert TypeChecker.which?(bool) == "boolean"
+    end
+  end
 end
